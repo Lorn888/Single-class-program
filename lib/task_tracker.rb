4c06005cc814851aa51ed13f7bd165a0d63f021f
Patryk <patrykokpara@hotmail.com>
2022-10-16 00:41:40 +0100

    Refactoring

--- lib/task_tracker.rb
+++ lib/task_tracker.rb
@@ -1,3 +1,19 @@
-def task_tracker(task)
-return task
+class Reminder
+
+    def initialize
+    @tasks = []
+    end
+
+    def task_tracker(task)
+        @tasks.push(task)
+    end
+
+    def task_list
+        return @tasks.join(", ")
+    end
 end
+
+ ok = Reminder.new
+
+ ok.task_tracker("task2")
+ p ok.task_tracker("task1")
--- spec/task_tracker_spec.rb
+++ spec/task_tracker_spec.rb
@@ -2,7 +2,9 @@ require 'task_tracker'
 
 RSpec.describe 'task_tracker' do
     it "has an option to add to do task" do
-        result = task_tracker("task1")
+        tracker = Reminder.new
+        tracker.task_tracker("task1")
+        result = tracker.task_list
         expect(result).to eq "task1"
     end 
 end
