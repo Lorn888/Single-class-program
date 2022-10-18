64a280428ada97574e485d88acd93b967fe32d2a
Patryk <patrykokpara@hotmail.com>
2022-10-18 23:37:30 +0100

    Wrote and passed 2nd test for complete method

--- spec/task_tracker_spec.rb
+++ spec/task_tracker_spec.rb
@@ -25,6 +25,18 @@ RSpec.describe 'task_tracker' do
         expect(result).to eq ""
     end
 
+    it "returns one task  " do
+        tracker = Reminder.new
+        tracker.task_tracker("task1")
+        tracker.task_tracker("task2")
+        tracker.complete
+        tracker.task_tracker("task3")
+        result = tracker.task_list
+        expect(result).to eq "task3"
+    end
+
+    
+
 
 
 end
